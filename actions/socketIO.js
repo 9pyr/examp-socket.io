fd4014b48aca148290b42c6be3fb74cbb40bbee2
Peeraphon Singla <ext.peeraphon.sin@truedigital.com>
2021-11-13 17:52:46 +0700

    test

--- actions/socketIO.js
+++ actions/socketIO.js
@@ -4,7 +4,7 @@ import { constantValue } from 'helpers/constant'
 
 const { event_emit } = constantValue
 
-const socket = io(process.env.NEXT_PUBLIC_BASE_URL)
+const socket = io()
 
 export const joinSocket = (room) => {
   console.log('Joining socket...', room)

a13d0bff0491c13b2faf149eb1d4fedc3ed5760f
9pyr <9pyra.s@gmail.com>
2021-11-13 16:18:09 +0700

    test commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Container, ListHorizontal, Button } from 'components/styles/styles.js'
 
 const HomePage = () => {
+  console.log('test commint')
   return (
     <Container>
       <div>Select user:</div>
